44403c1d9d264f9bb68a43196b2e3879576243a4
aldia_mechdream <futami16237@gmail.com>
2025-05-07 22:57:55 +0800

    chore: 在.cargo的文件夹下添加config以达到自定义命令的效果。直接在cargo.toml里加没有用；

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,35 @@
 use clap::Parser;
 use owo_colors::OwoColorize;
+use std::path::PathBuf;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    /// 列出归档内容
     #[arg(short, long)]
     list: bool,
 
+    /// 归档器操作日志
+    #[arg(short, long)]
+    log: bool,
+
+    /// 从归档中恢复指定文件或目录
     #[arg(short, long)]
     restore: Option<String>,
+
+    /// 要处理的文件目录地址，如果未指定其他命令，则对该目录进行移动操作
+    #[arg(value_name = "PATH")]
+    path: Option<PathBuf>,
 }
 
 fn main() {
     let args = Args::parse();
     if args.list {
         println!("现有列表：1,2,3");
-    }
-    if let Some(obj_path) = args.restore {
+    } else if let Some(obj_path) = args.restore {
         println!("要复原obj_path：{}", obj_path.yellow());
+    } else if let Some(path) = args.path {
+        println!("对目录 {} 进行移动操作", path.display().to_string().green());
+        // 这里实现移动目录的逻辑
     }
 }
