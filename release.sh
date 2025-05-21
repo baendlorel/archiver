14fbe8aa94a8cebb99f6dea4d199eb8dc266aa56
aldia_mechdream <futami16237@gmail.com>
2025-05-21 23:41:50 +0800

    chore: 修正artifact名字； release脚本不再折腾提交信息，反正也没有效果

--- release.sh
+++ release.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
-if [ $? -eq 0 ]; then
-  echo "请输入合并提交的信息："
-  read msg
-  git commit -m "$msg"
-fi
+# if [ $? -eq 0 ]; then
+#   echo "请输入合并提交的信息："
+#   read msg
+#   git commit -m "$msg"
+# fi
 # git checkout main
-# git merge --no-commit dev
+# git merge dev
 # git push
 git checkout release
-git merge --no-commit dev
-git commit -m "$msg"
+git merge dev
 git push
 git checkout dev
