9f586ec5f1d4d2231d9ba3861f566f32a7a509a3
aldia_mechdream <futami16237@gmail.com>
2025-05-24 00:04:00 +0800

    feat: 编写了用于保存jsonl的函数，计划用于jsonl文件的修改；

--- src/misc/jsonl.rs
+++ src/misc/jsonl.rs
@@ -1,11 +1,11 @@
 use crate::wrap_err_fatal;
 
 use serde::{Serialize, de::DeserializeOwned};
-use std::{fs::OpenOptions, io::Write, path::Path};
+use std::{fs, io::Write, path::Path};
 
 use crate::models::{error::ArchiverError, json_serde::JsonSerde};
 
-/// 给jsonl文件追加一行
+/// 给jsonl文件末尾追加一行
 pub fn append<T>(entry: &T, file_path: &Path) -> Result<(), ArchiverError>
 where
     T: ?Sized + Serialize + DeserializeOwned + JsonSerde,
@@ -14,15 +14,19 @@ where
     let json_line = wrap_err_fatal!(entry.to_json_line())?;
 
     // 以追加模式打开文件
-    let mut file = wrap_err_fatal!(OpenOptions::new().create(true).append(true).open(file_path))?;
+    let mut file = wrap_err_fatal!(
+        fs::OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open(file_path)
+    )?;
 
     // 写入
-    wrap_err_fatal!(file.write_all(json_line.as_bytes()))?;
-    wrap_err_fatal!(file.write_all(b"\n"))?;
-
+    wrap_err_fatal!(file.write_all(format!("{}\n", json_line).as_bytes()))?;
     Ok(())
 }
 
+/// 从jsonl文件加载列表
 pub fn load<T>(file_path: &Path) -> Result<Vec<T>, ArchiverError>
 where
     T: ?Sized + Serialize + DeserializeOwned + JsonSerde,
@@ -32,7 +36,7 @@ where
     }
 
     // 读取文件内容
-    let content = wrap_err_fatal!(std::fs::read_to_string(file_path))?;
+    let content = wrap_err_fatal!(fs::read_to_string(file_path))?;
 
     let mut list: Vec<T> = vec![];
     for line in content.lines() {
@@ -46,4 +50,23 @@ where
     }
 
     Ok(list)
+}
+
+/// 保存列表到jsonl文件
+pub fn save<T>(list: Vec<T>, file_path: &Path) -> Result<(), ArchiverError>
+where
+    T: ?Sized + Serialize + DeserializeOwned + JsonSerde,
+{
+    let mut content: Vec<String> = vec![];
+    for entry in list {
+        let l = wrap_err_fatal!(entry.to_json_line())?;
+        content.push(l);
+    }
+
+    wrap_err_fatal!(fs::write(
+        file_path,
+        format!("{}\n", content.join("\n")).as_bytes()
+    ))?;
+
+    Ok(())
 }
